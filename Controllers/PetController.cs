59958d69acd9ca9a16e442706e27faca8660f47a
Sidia <sidia.reis1@gmail.com>
2022-01-26 19:24:03 -0800

    init pet controller

---
+++ Controllers/PetController.cs
@@ -0,0 +1,16 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+//mapping for controller token
+[Route("[controller")]
+public class PetController: ControllerBase {
+
+    public PetController(){
+
+    }
+
+   
+
+}
