660aa0173e82ecf8ce5e91bb500afdd1655c1b4b
Sidia <sidia.reis1@gmail.com>
2022-01-26 19:31:50 -0800

    implements get methods in controller

--- Controllers/PetController.cs
+++ Controllers/PetController.cs
@@ -1,6 +1,8 @@
 
 
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+
 
 [ApiController]
 //mapping for controller token
@@ -10,7 +12,26 @@ public class PetController: ControllerBase {
     public PetController(){
 
     }
+    //List all Pets
+    [HttpGet]
+    public ActionResult<List<Pet>> GetAll() =>
+    
+           //return  result data in json
+        PetService.GetAll();
+  
+   
 
+    //get a single pet
+    [HttpGet("{id")]
+    public ActionResult<Pet> Get(int id)
+    {
+        var pet = PetService.Get(id);
+
+        if(pet == null)
+         return NotFound();
+
+         return pet;
+    }    
    
 
 }
